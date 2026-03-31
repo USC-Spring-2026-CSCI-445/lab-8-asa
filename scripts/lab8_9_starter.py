88630be3ff3ce81922f79b164d25db3bdbc4b39b
Amisha Nambiar <64343290+amishana@users.noreply.github.com>
2026-03-31 12:10:30 -0700

    Update lab8_9_starter.py

--- scripts/lab8_9_starter.py
+++ scripts/lab8_9_starter.py
@@ -480,14 +480,14 @@ class Controller:
                 if (count == 0): 
                     self.rotate_action(0.680678)
                     count += 1
-                elif (count < 9):
-                    self.forward_action(0.5)
+                elif (count < 20):
+                    self.forward_action(0.25)
                     count += 1
-                elif (count == 9):
+                elif (count == 20):
                     self.rotate_action(3.83972435)
                     count += 1
                 else:
-                    self.forward_action(0.5)
+                    self.forward_action(0.25)
 
             self.take_measurements()
             step += 1
