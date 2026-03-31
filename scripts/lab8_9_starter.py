a1e82b97f8cbf61e832e9f8d6877bc30888c999b
Amisha Nambiar <64343290+amishana@users.noreply.github.com>
2026-03-31 11:51:58 -0700

    Update lab8_9_starter.py

--- scripts/lab8_9_starter.py
+++ scripts/lab8_9_starter.py
@@ -478,7 +478,7 @@ class Controller:
             else:
                 
                 if (count == 0): 
-                    self.rotate_action(0.785398)
+                    self.rotate_action(0.698132)
                     count += 1
                 else: 
                     self.forward_action(0.3)
